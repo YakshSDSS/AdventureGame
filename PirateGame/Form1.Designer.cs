f9800b37a593c84df1e23d9e994648549ace43f9
YakshSDSS <yakspate551@ed.amdsb.ca>
2024-03-26 14:25:12 -0400

    Add project files.

---
+++ PirateGame/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PirateGame
+{
+    partial class pirateVoyage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pirateVoyage));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.Button();
+            this.option2 = new System.Windows.Forms.Button();
+            this.option3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("NSimSun", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(33, 9);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(221, 29);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Pirate Voyage";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox.BackgroundImage")));
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 46);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(251, 296);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("startButton.BackgroundImage")));
+            this.startButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.ForeColor = System.Drawing.Color.Black;
+            this.startButton.Location = new System.Drawing.Point(392, 146);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(161, 55);
+            this.startButton.TabIndex = 2;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("NSimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Turquoise;
+            this.outputLabel.Location = new System.Drawing.Point(282, 46);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(410, 155);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // option1
+            // 
+            this.option1.BackColor = System.Drawing.Color.Black;
+            this.option1.ForeColor = System.Drawing.Color.Turquoise;
+            this.option1.Location = new System.Drawing.Point(363, 220);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(209, 23);
+            this.option1.TabIndex = 4;
+            this.option1.Text = "..";
+            this.option1.UseVisualStyleBackColor = false;
+            this.option1.Click += new System.EventHandler(this.option1_Click);
+            // 
+            // option2
+            // 
+            this.option2.BackColor = System.Drawing.Color.Black;
+            this.option2.ForeColor = System.Drawing.Color.Turquoise;
+            this.option2.Location = new System.Drawing.Point(363, 259);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(209, 23);
+            this.option2.TabIndex = 5;
+            this.option2.Text = "..";
+            this.option2.UseVisualStyleBackColor = false;
+            this.option2.Click += new System.EventHandler(this.option2_Click);
+            // 
+            // option3
+            // 
+            this.option3.BackColor = System.Drawing.Color.Black;
+            this.option3.ForeColor = System.Drawing.Color.Turquoise;
+            this.option3.Location = new System.Drawing.Point(363, 299);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(209, 23);
+            this.option3.TabIndex = 6;
+            this.option3.Text = "..";
+            this.option3.UseVisualStyleBackColor = false;
+            this.option3.Click += new System.EventHandler(this.option3_Click);
+            // 
+            // pirateVoyage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(704, 354);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Font = new System.Drawing.Font("NSimSun", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "pirateVoyage";
+            this.Text = "Pirate Voyage";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1;
+        private System.Windows.Forms.Button option2;
+        private System.Windows.Forms.Button option3;
+    }
+}
+
---
+++ PirateGame/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PirateGame
+{
+    
+    public partial class pirateVoyage : Form
+    {
+        int page;
+
+        Random randGen = new Random();
+        new SoundPlayer introSound = new SoundPlayer(Properties.Resources.intro);
+        new SoundPlayer shipSound = new SoundPlayer(Properties.Resources.ship);
+        new SoundPlayer victorySound = new SoundPlayer(Properties.Resources.victory);
+        new SoundPlayer deathSound = new SoundPlayer(Properties.Resources.death);
+        new SoundPlayer fightSound = new SoundPlayer(Properties.Resources.fights);
+
+        public pirateVoyage()
+        {
+            InitializeComponent();
+
+            option1.Visible = false;
+            option2.Visible = false;
+            option3.Visible = false;
+
+            introSound.PlayLooping();
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            option1.Visible = true;
+            option2.Visible = true;
+            introSound.Stop();
+
+            page = 1;
+
+            DisplayPage();
+        }
+        private void option1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 12;
+            }
+            else if (page == 12)
+            {
+                page = 99;
+            }
+            else if (page == 3)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 12;
+            }
+            else if (page == 8)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+            else if (page == 13)
+            {
+                page = 14;
+            }
+            else if (page == 15)
+            {
+                page = 16;
+            }
+            else if (page == 16)
+            {
+                page = 19;
+            }
+            else if (page == 18)
+            {
+                page = 20;
+            }
+            else
+            {
+                page = 99;
+            }
+            DisplayPage();
+        }       
+        private void option2_Click(object sender, EventArgs e)
+        {
+            int randGenValue = randGen.Next(1, 11);
+            
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 6;
+            }
+            else if (page == 2)
+            {
+                if (randGenValue > 6)
+                {
+                    page = 4;
+                }
+                else
+                {
+                    page = 5;
+                }
+            }
+            else if (page == 7)
+            {
+                page = 13;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 15)
+            {
+                page = 17;
+            }
+            else if (page == 16)
+            {
+                page = 18;
+            }
+            else if (page == 18)
+            {
+                page = 19;
+            }
+            else if (page == 8)
+            {
+                page = 11;
+            }
+            else if (page == 9)
+            {
+                page = 7;
+            }
+            else
+            {
+                page = 1;
+            }
+            DisplayPage();
+        }
+        private void option3_Click(object sender, EventArgs e)
+        {
+            if (page == 3)
+            {
+                page = 8;
+            }
+            DisplayPage();
+        }
+        private void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You are a pirate with a 10 man crew. Your goal is to become rich.\nWhat do you do?";
+                    option1.Text = "Fight pirates";
+                    option2.Text = "Explore";
+                    pictureBox.BackgroundImage = (Properties.Resources.pirate_symbol);
+                    shipSound.PlayLooping();
+                    break;
+                case 2:
+                    outputLabel.Text = "You encounter a pirate crew of 15 men.\nDo you fight?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.pirate_crew);
+                    fightSound.PlayLooping();
+                    break;
+                case 3:
+                    option3.Visible = true;
+                    outputLabel.Text = "You come across a habited island.\nWhat do you do?";
+                    option1.Text = "Peacefully Visit";
+                    option2.Text = "Raid";
+                    option3.Text = "Keep exploring";
+                    pictureBox.BackgroundImage = (Properties.Resources.Island);
+                    shipSound.PlayLooping();
+                    break;
+                case 4:
+                    outputLabel.Text = "You kill them and steal their riches.\nYou are now very rich.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.Rich_image);
+                    victorySound.PlayLooping();
+                    break;
+                case 5:
+                    outputLabel.Text = "You get demolished by the enemies.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.DeathImage);
+                    deathSound.PlayLooping();
+                    break;
+                case 6:
+                    outputLabel.Text = "The villagers were Kung Fu masters and they kill you.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    option3.Visible = false;
+                    pictureBox.BackgroundImage = (Properties.Resources.DeathImage);
+                    deathSound.PlayLooping();
+                    break;
+                case 7:
+                    outputLabel.Text = "You come across an casino.\nDo you enter?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    option3.Visible = false;
+                    pictureBox.BackgroundImage = (Properties.Resources.Casino);
+                    break;
+                case 8:
+                    outputLabel.Text = "You never step foot on that island and leave.\nIt's been a while and you are low on supplies.";
+                    option1.Text = "Fish";
+                    option2.Text = "Phone marines";
+                    option3.Visible = false;
+                    pictureBox.BackgroundImage = (Properties.Resources.pirate_ship);
+                    shipSound.PlayLooping();
+                    break;
+                case 9:
+                    outputLabel.Text = "You get some more food.\nYou come across another island.\nWhat do you do?";
+                    option1.Text = "Keep exploring";
+                    option2.Text = "Visit";
+                    pictureBox.BackgroundImage = (Properties.Resources.pirate_ship);
+                    shipSound.PlayLooping();
+                    break;
+                case 10:
+                    outputLabel.Text = "You can't find land then you run out of food and starve to death.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.DeathImage);
+                    deathSound.PlayLooping();
+                    break;
+                case 11:
+                    outputLabel.Text = "The marines save you and throw you in jail where you\nrot for the rest of your life.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.DeathImage);
+                    deathSound.PlayLooping();
+                    break;
+                case 12:
+                    outputLabel.Text = "You gamble and become dirt poor.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.DeathImage);
+                    deathSound.PlayLooping();
+                    break;
+                case 13:
+                    outputLabel.Text = "You somehow find yourself at an auction.\nThere is a painting for $1000.\nDo you buy it?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    pictureBox.BackgroundImage = (Properties.Resources.Casino);
+                    break;
+                case 14:
+                    outputLabel.Text = "You later find out that the painting is worth quite a lot and\nsell it to become rich.";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    pictureBox.BackgroundImage = (Properties.Resources.Rich_image);
+                    victorySound.PlayLooping();
+                    break;
+                case 15:
+                    outputLabel.Text = "You explore the island more, looking for supplies.\nA massive gang robs you.\nWhat do you do?";
+                    option1.Text = "Fight";
+                    option2.Text = "Run";
+                    pictureBox.BackgroundImage = (Properties.Resources.Gang);
+                    break;
+                case 16:
+                    outputLabel.Text = "You lose a couple of men but get your money back.\nDo you follow the gang back to their hideout?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.Gang);
+                    fightSound.PlayLooping();
+                    break;
+                case 17:
+                    outputLabel.Text = "You are now dirt poor.\nPlay again? ";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.Gang);
+                    deathSound.PlayLooping();
+                    break;
+                case 18:
+                    outputLabel.Text = "You notice that they have lots of money laying around.\nDo you try and rob them?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    pictureBox.BackgroundImage = (Properties.Resources.Gang);
+                    fightSound.PlayLooping();
+                    break;
+                case 19:
+                    outputLabel.Text = "You do nothing with your life, you are now old and not rich.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.DeathImage);
+                    deathSound.PlayLooping();
+                    break;
+                case 20:
+                    outputLabel.Text = "You are the only one who survives and you become rich.\nPlay again?";
+                    option1.Text = "No";
+                    option2.Text = "Yes";
+                    pictureBox.BackgroundImage = (Properties.Resources.Rich_image);
+                    victorySound.PlayLooping();
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing!";
+                    option1.Text = "";
+                    option2.Text = "";
+
+                    option1.Visible = false;
+                    option2.Visible = false;
+
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+            }
+        }
+    }
+}
+
